cd93122642dc0727b27c292eb7cc4fd812dfeaa4
Samuel <samuelajala01@gmail.com>
2024-09-23 22:26:02 +0100

    init: project scaffold

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+    return ( <><Footer><h1>This is the footer</h1></Footer></> );
+}
+ 
+export default Footer;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+const Navbar = () => {
+    return ( <><div><ul>
+       <li>Home</li>
+       <li>About</li>
+       <li> Events</li>
+       <li>sReources</li>
+        </ul></div></> );
+}
+ 
+export default Navbar;
