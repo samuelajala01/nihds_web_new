003fafc5da4d2ff7d372bbd473bb5facf4d5b18d
Samuel <samuelajala01@gmail.com>
2024-10-02 22:38:00 +0100

    chore: add hero bg image

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
-import Footer from "@/Components/Footer";
 import SliderDiv from "@/Components/SliderDiv";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <section className="bg-blue-800 m-auto text-center p-8">
+    <section className="bg-[url('../../public/assets/hero.png')] m-auto text-center p-8">
       <h1 className="text-4xl md:text-[5vw] font-semibold mb-8 w-fit lg:mx-48 leading-tight">Empowering Innovators. Bridging Challenges. Building Community</h1>
       <p className="w-7/8 sm:w-3/5 md:w-1/2 m-auto my-8">Join a thriving network of hardware developers, founders, and AI engineers. Connect, collaborate, and unlock new opportunities</p>
 
