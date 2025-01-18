8c3467876d3569d689bcb705dc77791135113219
Samuel <samuelajala01@gmail.com>
2025-01-18 23:38:21 +0100

    style: make navbar absolute

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,19 +3,19 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <>
-      <div className="flex mx-auto gap-[4vw] m-auto justify-around p-6 bg-green-300 w-fit fixed ">
-        {/* <div>
+      <div className="flex mx-auto gap-[4vw] justify-around p-6 bg-green-300 w-fit absolute mt-8 left-[25vw]">
+        <div>
           <Image alt="Logo" className="" width={60} height={40} />
-        </div> */}
-        <ul className="flex gap-[4vw]">
+        </div>
+        <ul className="flex gap-[4vw] items-center">
           <li>Home</li>
           <li>About</li>
           <li>Events</li>
           <li>Reources</li>
         </ul>
-        {/* <button className="bg-[#03045E] text-white rounded-md py-2 px-6">
+        <button className="bg-[#03045E] text-white rounded-md py-2 px-6">
           Join Now
-        </button> */}
+        </button>
       </div>
     </>
   );
