4e620d43ef7a0d2a449d8fef2981970233358e69
Samuel <samuelajala01@gmail.com>
2025-01-19 18:25:16 +0100

    style: add nav border and logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Logo from "../../public/assets/logo.png";
 
 const Navbar = () => {
   return (
     <>
-      <div className="flex mx-auto gap-[4vw] justify-around p-6 bg-green-300 w-fit absolute mt-8 left-[25vw]">
+      <div className="flex mx-auto gap-[4vw] justify-around py-6 px-8 bg-white w-fit absolute mt-8 left-[25vw] rounded-full">
         <div>
-          <Image alt="Logo" className="" width={60} height={40} />
+          <Image alt="Logo" className="" width={120} height={120} src={Logo} />
         </div>
         <ul className="flex gap-[4vw] items-center">
           <li>Home</li>
