4410b02df4c2ed6a7efd254f2e95f22ea3f9428f
Samuel <samuelajala01@gmail.com>
2025-01-23 13:41:01 +0100

    chore: add contend text to about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,30 @@
 const About = () => {
-    return ( <></> );
-}
- 
+  return (
+    <>
+      <section>
+        <h1>Empowering Innovations through Collaborations</h1>
+        <p>
+          Discover how we bring together visionary minds to shape the future of
+          technology, solving real-world challenges and creating opportunities
+          for all.
+        </p>
+      </section>
+      <section>
+        <blockquote>
+          “By coming together, we unlock the strength to overcome challenges and
+          create new opportunities. It's through collaboration and shared effort
+          that we achieve more than we ever could alone. This is the real power
+          of community.”
+        </blockquote>
+        <div>
+            <div></div>
+            <div>
+                <p>We are a vibrant and inclusive community aimed at bridging the gap and overcoming the challenges faced by hardware developers, startup founders, entrepreneurs, and AI engineers in Nigeria. Our platform serves as a collaborative space that fosters innovation, knowledge sharing, and mutual support among individuals and entities involved in the hardware, AI, and IoT industries. By providing a supportive environment, access to resources, and opportunities for networking, It empowers its members to transform their ideas into successful ventures, driving technological advancement and economic growth in Nigeria.</p>
+            </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
 export default About;
